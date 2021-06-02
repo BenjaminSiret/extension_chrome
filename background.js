a80c544751d27a933f43bfc4eda117d570f13320
Benjamin Siret <benjaminsiret@outlook.fr>
2021-06-02 18:42:33 +0200

    first commit

---
+++ background.js
@@ -0,0 +1,9 @@
+
+
+// list all the tabs in active window, then display there ids
+// chrome.tabs.query({}, (tabs) => {
+//   for (let i = 0; i < tabs.length; i++) {
+//     console.log(tabs[i].id)
+//   }
+// });
+
