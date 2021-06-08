01ab408ab391e322a3dc8ce0a93db1ebba069310
Benjamin Siret <benjaminsiret@outlook.fr>
2021-06-08 11:24:13 +0200

    group tabs, get the groupId

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+let tabs = [2,84,85]
+let principal = chrome.tabs.group({tabIds : tabs});
+
+
+
 chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
   document.write(`<h3>Voici les onglets ouverts :</h3>`);
   document.write('<ul>');
   for (let i = 0; i < tabs.length; i++) {
-    document.write(`<li>${tabs[i].url}</li>`);
+    document.write(`<li>url : ${tabs[i].url}</li>`);
+    document.write(`<li>id de la tab : ${tabs[i].id}</li>`);
+    document.write(`<li>id du groupe : ${tabs[i].groupId}</li>`);
+    document.write(`-----------------`);
   }
   document.write('</ul>');
 });
