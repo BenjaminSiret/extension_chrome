75941af8db24bb386a65232af80c1b1a56d7fa09
Benjamin Siret <benjaminsiret@outlook.fr>
2021-06-15 18:08:46 +0200

    add function addTabToGroup and whichTabs

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-// liste en dur des ids des tabs que je mets dans le groupe
-let tabList = []
+// création de groupes en dur pour tester
+let sportGroup = { category: "sport", tabList: [1,3,54] };
+let principalGroup = { category: "principal", tabList: [2,7,45] };
 
-// fonction d'ajout de l'id de la nouvelle tab
-function newTab() {
-  chrome.tabs.onCreated.addListener((tab) => {
-  tabList.push(tab.id);
-  console.log(tabList);
-})}
+// définition de la fonction qui ajouter une tab à un groupe de tabs
+function addTabToGroup(group, tabId) {
+  group.tabList.push(tabId);
+}
 
-newTab();
+// appel de la fonction addTabToGroup et test
+addTabToGroup(sportGroup, 7);
+console.log(sportGroup.tabList);
 
-// fonction qui groupe chaque tab à sa création
-function groupTabs(list) {
-  chrome.tabs.onCreated.addListener(() => {
-    chrome.tabs.group({tabIds : list});
-    console.log("coucou");
-  })}
+// définition de la fonction qui permet d'accéder aux ids des onglets d'un groupe donné
+function whichTabs(group) {
+  return group.tabList.join(", ");
+}
 
-groupTabs(tabList);
+let sportList = whichTabs(sportGroup);
+console.log(sportList);
 
-chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-  document.write(`<h3>Voici les onglets ouverts :</h3>`);
-  document.write('<ul>');
-  for (let i = 0; i < tabs.length; i++) {
-    document.write(`<li>url : ${tabs[i].url}</li>`);
-    document.write(`<li>id de la tab : ${tabs[i].id}</li>`);
-    document.write(`<li>id du groupe : ${tabs[i].groupId}</li>`);
-    document.write(`-----------------`);
-  }
-  document.write('</ul>');
-});
+// affichage des informations sur les onglets ouverts
+// chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+//   document.write(`<h3>Voici les onglets ouverts :</h3>`);
+//   document.write('<ul>');
+//   for (let i = 0; i < tabs.length; i++) {
+//     document.write(`<li>url : ${tabs[i].url}</li>`);
+//     document.write(`<li>id de la tab : ${tabs[i].id}</li>`);
+//     document.write(`<li>id du groupe : ${tabs[i].groupId}</li>`);
+//     document.write(`-----------------`);
+//   }
+//   document.write('</ul>');
+// });
