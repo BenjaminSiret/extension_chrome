fe71800061cd03505a86c6a150576d2caac516c3
Benjamin Siret <benjaminsiret@outlook.fr>
2021-06-18 19:34:00 +0200

    push real ids in tabList

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
-// création de groupes en dur pour tester
-let sportGroup = { category: "sport", tabList: [2,48,49] };
-let principalGroup = { category: "principal", tabList: [2,7,45] };
+// création de groupe en dur pour tester
+let sportGroup = { category: "sport", tabList: [] };
+// je remplace la tabList en dur par les vrais ids des tabs
+chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+  for (let i = 0; i < tabs.length; i++) {
+    sportGroup.tabList.push(tabs[i].id);
+  }
+  console.log(sportGroup.tabList);
+})
 
+
+//test de création d'objet groupe
 const group = { name: "group", tabList: [] };
-
 const educGroup = Object.create(group);
 educGroup.name = "educ";
 educGroup.tabList = [6,14];
 
-// affichage des tabs d'un groupe
-// j'itère sur la tabList du groupe et j'affiche chaque tab pour l'id correspondant
-sportGroup.tabList.forEach(id => {
-  console.log(id);
 
-});
-
-console.log("............");
-
-principalGroup.tabList.forEach(id => {
-  console.log(id);
-}
-
-)
 // définition de la fonction qui ajoute une tab à un groupe de tabs
-// function addTabToGroup(group, tabId) {
-//   group.tabList.push(tabId);
-// }
-// // appel de la fonction addTabToGroup et test
-// addTabToGroup(sportGroup, 7);
-// console.log(sportGroup.tabList);
+function addTabToGroup(group, tabId) {
+  group.tabList.push(tabId);
+}
+// appel de la fonction addTabToGroup et test
+addTabToGroup(sportGroup, 7);
+console.log(sportGroup.tabList);
 
-// // définition de la fonction qui permet d'accéder aux ids des onglets d'un groupe donné
-// function whichTabs(group) {
-//   return group.tabList.join(", ");
-// }
-// // appel de la fonction whichTabs et test
-// let sportList = whichTabs(sportGroup);
-// console.log(sportList);
+// définition de la fonction qui permet d'accéder aux ids des onglets d'un groupe donné
+function whichTabs(group) {
+  return group.tabList.join(", ");
+}
+// appel de la fonction whichTabs et test
+let sportList = whichTabs(sportGroup);
+console.log(sportList);
 
 
 // // affichage des informations sur les onglets ouverts
